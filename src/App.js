0e5c5ddc8ef75857eebc7a51b8af4bfed1d96650
shanegracias <shanegracias7@gmail.com>
2022-02-26 11:59:46 +0530

    storing to do item in local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React,{ useState , useRef } from "react";
+import React,{ useState , useRef , useEffect} from "react";
 import ToDoList from "./ToDoList";
 import  { v4 as uuidv4 } from 'uuid'
 
 function App() {
   const [storedToDos, setToDos] = useState([])
   const toDoRef = useRef()
+  const LocalStorageKey = 'todo.local'
+
+  useEffect(()=>{
+    const localStorageToDo = JSON.parse(localStorage.getItem(LocalStorageKey)) 
+    if(localStorageToDo) setToDos(localStorageToDo)
+  },[])
+  useEffect(()=>{
+    localStorage.setItem(LocalStorageKey,JSON.stringify(storedToDos))
+  },[storedToDos])
 
   function handleAdd(e){
     const value = toDoRef.current.value
